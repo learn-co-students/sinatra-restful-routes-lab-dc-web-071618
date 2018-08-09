b34f642dcdae3f5d38fb3ea7389a41c366fa4f30
github username <github email address>
2018-08-08 20:14:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,69 @@
+require "pry"
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  set :method_override, true
+
+
   get '/' do
+    # erb :index
+  end
+
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+
+  post '/recipes' do
+    @recipe = Recipe.new
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id/delete' do
+    Recipe.find(params[:id]).destroy
+    redirect '/recipes'
+  end
+
+  patch '/recipes/:id' do
+    changed = Recipe.find(params[:id])
+    changed.name = params[:name]
+    changed.ingredients = params[:ingredients]
+    changed.cook_time = params[:cook_time]
+    changed.save
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    Recipe.find(params[:id]).destroy
+    redirect '/recipes'
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+
 end
